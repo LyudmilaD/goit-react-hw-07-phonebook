7289291698a5c3061f00d1ab933431e6bd70d4e2
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-07-02 02:54:28 +0300

    list of contacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,24 +3,26 @@ import Contact from '../ContactElementList/ContactElementList';
 import { useGetContactsQuery } from 'contactStorage/contactAPI';
 import styles from '../Form/Form.module.css';
 
-const getContacts = () => {
-  if (filter === '') {
-    return contacts;
-  }
-
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-};
 export const ContactList = () => {
   const { data: contacts, isSuccess } = useGetContactsQuery();
   const filter = useSelector(state => state.filter);
+
+  const getContacts = () => {
+    if (filter === '') {
+      return contacts;
+    }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
   const items = getContacts();
+
   return (
     <ul className={styles.wrap}>
       {isSuccess &&
         items.map(({ id, name, phone }) => (
-          <Contact key={id} id={id} name={name} number={number} />
+          <Contact key={id} id={id} name={name} number={phone} />
         ))}
       {items && items.length === 0 && (
         <span className={styles.button}> No contacts </span>
